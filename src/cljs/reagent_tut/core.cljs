3d01cfa5e5aece409353a452d6730af651861e0e
James Cagalawan <ajcagala@uwaterloo.ca>
2015-03-10 06:24:52 -0400

    Added CommentList and CommentForm reagent components and added them to the CommentBox.

--- src/cljs/reagent_tut/core.cljs
+++ src/cljs/reagent_tut/core.cljs
@@ -1,10 +1,19 @@
 (ns reagent-tut.core
     (:require [reagent.core :as reagent]))
 
+(defn comment-list []
+  [:div {:className "commentList"}
+   "Hello, world! I am a CommentList"])
+
+(defn comment-form []
+  [:div {:className "commentForm"}
+   "Hello, world! I am a CommentForm"])
+
 (defn comment-box [] 
-  [:div 
-   {:className "commentBox"} 
-   "Hello, world! I am a CommentBox."])
+  [:div {:className "commentBox"} 
+   [:h1 "Comments"]
+   [comment-list]
+   [comment-form]])
 
 (reagent/render-component [comment-box]
                           (.getElementById js/document "app"))
