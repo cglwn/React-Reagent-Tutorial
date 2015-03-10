6c10bb3982cac77512790ce93931adfe5af28151
James Cagalawan <ajcagala@uwaterloo.ca>
2015-03-10 06:48:41 -0400

    Added Comment reagent component and some test data.

--- src/cljs/reagent_tut/core.cljs
+++ src/cljs/reagent_tut/core.cljs
@@ -1,9 +1,15 @@
 (ns reagent-tut.core
     (:require [reagent.core :as reagent]))
 
+(defn comment [author text]
+  [:div {:className "comment"}
+   [:h2 {:className="commentAuthor"} author]
+   text])
+
 (defn comment-list []
   [:div {:className "commentList"}
-   "Hello, world! I am a CommentList"])
+   [comment "Pete Hunt" "This is one comment"]
+   [comment "Jordan Walke" "This is *another* comment"]])
 
 (defn comment-form []
   [:div {:className "commentForm"}
