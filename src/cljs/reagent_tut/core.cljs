47834f70c795e72f9dfb0c3792c96abcf69f6924
James Cagalawan <ajcagala@uwaterloo.ca>
2015-03-10 06:08:22 -0400

    Initial commit

---
+++ src/cljs/reagent_tut/core.cljs
@@ -0,0 +1,10 @@
+(ns reagent-tut.core
+    (:require [reagent.core :as reagent]))
+
+(defn home []
+  [:div [:h1 "Welcome to Reagent Cookbook!"]
+   
+   ])
+
+(reagent/render-component [home]
+                          (.getElementById js/document "app"))
